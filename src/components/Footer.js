522ae55d2aae24130000cb1de4f38b8cd871349c
desaiHarsh08 <desaiharsh183@gmail.com>
2023-09-16 12:08:27 +0530

    fourth commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='py-5 fixed w-full bottom-0 text-center uppercase '>
+    <footer className='py-5 text-[12px] fixed w-full bottom-0 text-center uppercase '>
       <p>&copy; 2023 - The Bhawanipur Education Society College</p>
     </footer>
   )
